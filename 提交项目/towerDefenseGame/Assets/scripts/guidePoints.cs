759faf2a41781343d867ec766c9f6024fa0f9238
Ernie <jasonernie03@gmail.com>
2020-11-28 10:32:14 -0500

    add guide points, the zombie is able to move

---
+++ 提交项目/towerDefenseGame/Assets/scripts/guidePoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class guidePoints : MonoBehaviour
+{
+    public static Transform[] guidePointsArray;
+
+    private void Awake()
+    {
+        guidePointsArray = new Transform[transform.childCount];
+        for (int i = 0; i < guidePointsArray.Length; i++)
+        {
+            guidePointsArray[i] = transform.GetChild(i);
+        }
+    }
+
+}
---
+++ 提交项目/towerDefenseGame/Assets/scripts/zombie.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class zombie : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    private Transform zombieCurrentMoveTo;  // guidePoint the zombie currently is moving to 
+    private int currentMoveToIndex; // index for the zombie currently is moving to 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zombieCurrentMoveTo = guidePoints.guidePointsArray[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = zombieCurrentMoveTo.position - transform.position;
+        transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, zombieCurrentMoveTo.position) <= 0.2f)
+        {
+            nextGuidePointInfo();
+
+        }
+    }
+
+    void nextGuidePointInfo() { // get the next guide point the zombie should move to 
+        if (currentMoveToIndex >= guidePoints.guidePointsArray.Length - 1) {
+            Destroy(gameObject);
+            return;
+        }
+        currentMoveToIndex++;
+        zombieCurrentMoveTo = guidePoints.guidePointsArray[currentMoveToIndex];
+    }
+}
