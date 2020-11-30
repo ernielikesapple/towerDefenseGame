fcddf6cce3e9f006011a32769f948769f35a3fa4
Ernie <jasonernie03@gmail.com>
2020-11-29 19:45:31 -0500

    now user can place the turret on the selected tile in iOS device landscape mode

---
+++ 提交项目/towerDefenseGame/Assets/scripts/ConstructManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructManager : MonoBehaviour
+{
+	public static ConstructManager instance;
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			return;
+		}
+		instance = this;
+	}
+
+	public GameObject buildEffect;
+	public GameObject sellEffect;
+
+	private TurretToBuild turretToBuild;
+	private Tile selectedNode;
+
+	public GameObject testprefab;
+
+	//public NodeUI nodeUI;
+
+	public bool CanBuild { get { return turretToBuild != null; } }
+	//public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } } // todo: update play stats
+
+	public void SelectNode(Tile tile)
+	{
+		if (selectedNode == tile)
+		{
+			DeselectNode();
+			return;
+		}
+
+		selectedNode = tile;
+		turretToBuild = null;
+
+		//nodeUI.SetTarget(tile); //todo: update nodeUI
+	}
+
+	public void DeselectNode()
+	{
+		selectedNode = null;
+		//nodeUI.Hide(); //todo: update nodeUI
+	}
+
+	public void SelectTurretToBuild(TurretToBuild turret)
+	{
+		turretToBuild = turret;
+		DeselectNode();
+	}
+
+	public TurretToBuild GetTurretToBuild()
+	{
+		return turretToBuild;
+	}
+}
--- 提交项目/towerDefenseGame/Assets/scripts/Tile.cs
+++ 提交项目/towerDefenseGame/Assets/scripts/Tile.cs
@@ -6,8 +6,15 @@ public class Tile : MonoBehaviour
 {
     public Color hoverIndicateColor;
 
+    public Vector3 positionAboveTile;
+
+    [HideInInspector]
+    public GameObject turretOnTile;
+
     private Renderer nodeRender;
     private Color orginalColor;
+
+    ConstructManager constructManager;
 
     void Start()
     {
@@ -16,12 +23,29 @@ public class Tile : MonoBehaviour
 
     }
 
+    void OnMouseDown()
+    {
+        if (turretOnTile != null)
+        {
+            //buildManager.SelectNode(this);
+            Debug.Log("already have a turret, but we can upgrate the turrent");
+            return;
+        }
+
+        //if (!constructManager.CanBuild)
+        //    return;
+        GameObject testT = ConstructManager.instance.testprefab;
+        turretOnTile = (GameObject)Instantiate(testT, transform.position + positionAboveTile, transform.rotation);
+        //BuildTurret(constructManager.GetTurretToBuild());
+
+    }
+
     void OnMouseEnter()
     {
         nodeRender.material.color = hoverIndicateColor;
     }
 
-    private void OnMouseExit()
+    void OnMouseExit()
     {
         nodeRender.material.color = orginalColor;
     }
